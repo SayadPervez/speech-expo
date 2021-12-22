f6774aa4962f08663c33a929cabef5f24b110dd6
Sayad Pervez <pervez2504@gmail.com>
2021-12-23 00:17:23 +0530

    Initial Commit with working controller

---
+++ controller.py
@@ -0,0 +1,15 @@
+from AMD import ardata
+import sys
+
+if(len(sys.argv)==2):
+    msg=(str(sys.argv[1]).lower()).split(" ")
+else:
+    print("No CLA")
+    exit()
+
+if("light" in msg or "lights" in msg or "lite" in msg) and ("on" in msg) and ("turn" in msg):
+    rawData = ardata(4,dynamic=True,msg='l-on',lines=1,squeeze=False,numeric=False)
+elif("light" in msg or "lights" in msg or "lite" in msg) and ("of" in msg or "off" in msg) and ("turn" in msg):
+    rawData = ardata(4,dynamic=True,msg='l-off',lines=1,squeeze=False,numeric=False)
+else:
+    print("invalid")
