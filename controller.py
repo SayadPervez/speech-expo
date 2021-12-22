39d20f6323f1c66b6c8b6dba8cad492ff003f88d
Sayad Pervez <pervez2504@gmail.com>
2021-12-23 00:19:43 +0530

    gg

--- controller.py
+++ controller.py
@@ -7,9 +7,11 @@ else:
     print("No CLA")
     exit()
 
+com = 4
+
 if("light" in msg or "lights" in msg or "lite" in msg) and ("on" in msg) and ("turn" in msg):
-    rawData = ardata(4,dynamic=True,msg='l-on',lines=1,squeeze=False,numeric=False)
+    rawData = ardata(com,dynamic=True,msg='l-on',lines=1,squeeze=False,numeric=False)
 elif("light" in msg or "lights" in msg or "lite" in msg) and ("of" in msg or "off" in msg) and ("turn" in msg):
-    rawData = ardata(4,dynamic=True,msg='l-off',lines=1,squeeze=False,numeric=False)
+    rawData = ardata(com,dynamic=True,msg='l-off',lines=1,squeeze=False,numeric=False)
 else:
     print("invalid")
