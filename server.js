1428014da4dc0811a5a86045a7c839c5f8c26311
Sayad Pervez <pervez2504@gmail.com>
2021-12-23 22:15:46 +0530

    pseudo initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*:*"
+  }
+});
+app.use(express.static('./public'));
+
+io.on('connection', (socket) => {
+    //testing
+    console.log("Connect "+String(socket.id))
+    socket.on('run', (msg) => {
+      cmdline(msg);
+    });
+
+    function cmdline(command){
+      const exec = require('child_process').exec;
+      exec(`${command}`, { encoding: 'utf-8' }, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}, ${stderr}`);
+          io.to(socket.id).emit("subprocess_out","Error");
+        }else{
+        console.log(stdout);
+        io.to(socket.id).emit("subprocess_out","Success");
+        }
+      });
+    }
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
