36108719589b5072900d9d6761c39975f8e41cd2
Sayad Pervez <pervez2504@gmail.com>
2021-12-23 23:37:48 +0530

    final commit

--- controller.py
+++ controller.py
@@ -9,9 +9,5 @@ else:
 
 com = 4
 
-if("light" in msg or "lights" in msg or "lite" in msg) and ("on" in msg) and ("turn" in msg):
-    rawData = ardata(com,dynamic=True,msg='l-on',lines=1,squeeze=False,numeric=False)
-elif("light" in msg or "lights" in msg or "lite" in msg) and ("of" in msg or "off" in msg) and ("turn" in msg):
-    rawData = ardata(com,dynamic=True,msg='l-off',lines=1,squeeze=False,numeric=False)
-else:
-    print("invalid")
+print(msg)
+rawData = ardata(com,dynamic=True,msg=msg[0],lines=1,squeeze=False,numeric=False)
--- middleman.py
+++ middleman.py
@@ -11,9 +11,9 @@ msg=(" ".join(msg)).lower()
 print(msg)
 if((("motor" in msg or "mota" in msg) and ("forward" in msg or "back" in msg or "stop" in msg or "top" in msg or "reverse" in msg))):
     if("forward" in msg):
-        os.system('python controller.py "forward"')
+        os.system('python controller.py "forwards"')
     elif("back" in msg or "reverse" in msg):
-        os.system('python controller.py "reverse"')
+        os.system('python controller.py "backwards"')
     elif("top" in msg or "stop" in msg):
         os.system('python controller.py "stop"')
     print("success from python")
